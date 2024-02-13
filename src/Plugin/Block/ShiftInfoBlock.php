30fdc1cb863cb7802715c5fd618a89c743fb254c
theo <theo@jlab.org>
2024-02-13 10:32:30 -0500

    fetches shiftinfo block data from epicswebops

---
+++ src/Plugin/Block/ShiftInfoBlock.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Drupal\cebaf_status\Plugin\Block;
+
+use Drupal\cebaf_status\Plugin\fetcher\ContentFetcher;
+use Drupal\Core\DependencyInjection\Container;
+
+/**
+ * Provides a 'Contact Info' Block.
+ *
+ * @Block(
+ *   id = "shift_info",
+ *   admin_label = @Translation("Shift Info block"),
+ *   category = @Translation("CEBAF"),
+ * )
+ */
+class ShiftInfoBlock extends \Drupal\Core\Block\BlockBase
+{
+
+  /**
+   * The base caget url
+   */
+  const url = 'https://epicswebops.acc.jlab.org/epics2web/caget';
+
+  /**
+   * The pvs that hold shift information.
+   */
+  const pvs = [
+    'shift' => 'comm1',
+    'crewChief' => 'comm2',
+    'operators' => 'comm3',
+    'programDeputy' => 'comm5',
+    'operationsProgram' => 'comm6',
+    'physicsProgram' => 'comm7',
+    'announcements' => 'comm8'
+  ];
+
+
+  /**
+   * @inheritDoc
+   */
+  public function build()
+  {
+    return [
+      '#title' => 'Shift Information',
+      '#attributes' => ['class' => ['block-block-content']],  // to match standard block class css
+      'content' => $this->content(),
+      '#wrapper_attributes' => ['class' => 'container'],
+    ];
+  }
+
+  /**
+   *
+   * @return string
+   */
+  protected function url(): string
+  {
+    return self::url . '?pv=' . implode('&pv=', array_values(self::pvs));
+  }
+
+  protected function data()
+  {
+    $client = \Drupal::service('http_client_factory');
+    $logger = \Drupal::service('logger.factory')->get('cebaf');
+    $messenger = \Drupal::service('messenger');
+    $fetcher = new ContentFetcher($client, $logger, $messenger);
+    return $fetcher->fetch($this->url());
+  }
+
+  protected function pvValues()
+  {
+    $decoded = json_decode($this->data());
+    return collect($decoded->data)->pluck('value', 'name')->all();
+  }
+
+  protected function content()
+  {
+    return [
+      'table' => [
+        '#type' => 'table',
+        '#attributes' => ['class' => ['table', 'table-striped']],
+        '#rows' => $this->contentRenderRows()
+      ]
+    ];
+  }
+
+  /**
+   * Shift data as an array of rows in Drupal render array format.
+   *
+   * @return array
+   */
+  protected function contentRenderRows()
+  {
+    $rows = [];
+    foreach ($this->contentArray() as $key => $value) {
+      $rows[] = [
+          ['data' =>  $key, 'header' => TRUE],
+          ['data' => $value],
+      ];
+    }
+    return $rows;
+  }
+
+  /**
+   * An array containing the data with keys as they will be shown on the web.
+   * @return array
+   */
+  protected function contentArray(): array
+  {
+    $pvValues = $this->pvValues();
+    return [
+      'Shift' => $pvValues[self::pvs['shift']],
+      'CEBAF Program' => $pvValues[self::pvs['physicsProgram']],
+      'MCC Crew Chief' => $pvValues[self::pvs['crewChief']],
+      'MCC Operators' => $pvValues[self::pvs['operators']],
+      'Program Deputy' => $pvValues[self::pvs['programDeputy']],
+      'Announcements' => $pvValues[self::pvs['announcements']],
+    ];
+  }
+
+}
+
---
+++ src/Plugin/fetcher/ContentFetcher.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Drupal\cebaf_status\Plugin\fetcher;
+
+use Drupal\Core\Http\ClientFactory;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use GuzzleHttp\Exception\TransferException;
+use GuzzleHttp\Psr7\Request;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a default fetcher implementation.
+ *
+ * Uses the http_client service to download the feed.
+ *
+ * @AggregatorFetcher(
+ *   id = "aggregator",
+ *   title = @Translation("Default fetcher"),
+ *   description = @Translation("Downloads data from a URL using Drupal's HTTP request handler.")
+ * )
+ */
+class ContentFetcher {
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \Drupal\Core\Http\ClientFactory
+   */
+  protected $httpClientFactory;
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+
+  /**
+   * Constructs a ContentFetcher object.
+   *
+   * @param \Drupal\Core\Http\ClientFactory $http_client_factory
+   *   A Guzzle client object.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(ClientFactory $http_client_factory, LoggerInterface $logger, MessengerInterface $messenger) {
+    $this->httpClientFactory = $http_client_factory;
+    $this->logger = $logger;
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('http_client_factory'),
+      $container->get('logger.factory')->get('cebaf'),
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fetch($url) {
+    $request = new Request('GET', $url);
+
+    try {
+      $response = $this->httpClientFactory->fromOptions(['verify' => FALSE])->send($request);
+      $content = (string) $response->getBody();
+      $this->logger->info($content);
+      return $content;
+    }
+    catch (TransferException $e) {
+      $this->logger->warning('The content from %url seems to be broken because of error "%error".', [
+        '%url' => $url,
+        '%error' => $e->getMessage(),
+      ]);
+      $this->messenger->addWarning(new TranslatableMarkup('The content from %url seems to be broken because of error "%error".', [
+        '%url' => $url,
+        '%error' => $e->getMessage(),
+      ]));
+      return FALSE;
+    }
+  }
+}
